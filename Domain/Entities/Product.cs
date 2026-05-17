e2caf7388c547c73b559b59e6e8ae4fae9506532
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-05-18 00:53:29 +0200

    adding user model and product model

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    internal class Product
+    {
+
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Category { get; set; }
+
+
+        //        •	- ID(GUID)
+        //•	- Name(string)
+        //•	- Description(string)
+        //•	- Price(decimal)
+        //•	- Category(string)
+
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.Entities
+{
+    public class User : IdentityUser
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string FirsName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        [Phone]
+
+        public string phoneNumber { get; set; }
+
+        public string email { get; set; }
+
+
+        public DateOnly  DateofBirth  { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string Password { get; set; }
+    }
+}
